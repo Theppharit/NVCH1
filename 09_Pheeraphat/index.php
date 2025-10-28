65502798a3fbbb9d90f62ba3c2774e52e1c09355
Pheeraphat01 <kubibo007@gmail.com>
2025-10-28 17:13:21 +0700

    Update index.php

--- 09_Pheeraphat/index.php
+++ 09_Pheeraphat/index.php
@@ -6,6 +6,9 @@
     <title>Pheeraphat</title>
 </head>
 <body>
-    
+    <h1 class="">Pheeraphat Koophimai</h1>
+    <h2 class="">IT</h2>
+    <h3 class="">NVC</h3>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati at optio ipsam temporibus similique maiores aliquam possimus autem deleniti esse impedit labore maxime cum aliquid animi consequatur soluta, dolorem recusandae.</p>
 </body>
 </html>
