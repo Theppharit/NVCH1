b96bab80cce089e19530d0f7dbb2f58e8e807d07
tonkr0611044154-wq <tonkr0611044154@gmail.com>
2025-10-28 17:05:58 +0700

    Update index.php

--- 08_Dechathorn/index.php
+++ 08_Dechathorn/index.php
@@ -10,7 +10,28 @@
 
 
 <body>
-<h1 class="">kar age 19</h1><p>high 165</p><p>weigh 72</p>
+
+<nav class=""></nav>
+
+<main class="">
+
+<section class="" id="">
+<h1 class="">dechathorn</h1>
+<h1 class="">kar</h1>
+<h1 class="">age 19</h1>
+<p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident repudiandae asperiores quidem minus quia nesciunt in aliquam aperiam, maiores sunt! Sapiente quis necessitatibus nesciunt veritatis fugit ad distinctio facere harum!
+Molestiae reiciendis impedit repudiandae dolores cupiditate sed, labore sunt. Adipisci quo nihil consequuntur, incidunt provident voluptatum. Unde magni voluptate, velit omnis nulla iste. Veritatis voluptate impedit id ut neque eius!</p>
+
+</section>
+
+
+</main>
+
+
+
+<footer class=""></footer>
+
+
 </body>
 
 </html>
