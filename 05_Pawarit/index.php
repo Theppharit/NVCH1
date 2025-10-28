ba30096fe0449d60921449e4677ab2a44b94d163
mahathepsin <studyno1mahathep@gmail.com>
2025-10-28 17:15:24 +0700

    .

--- 05_Pawarit/index.php
+++ 05_Pawarit/index.php
@@ -9,9 +9,27 @@
 
 
 </head>
+
+
 <body>
     
+<nav class=""></nav>
 
+
+<main class="">
+
+
+<section class="" id="">
+
+
+<h1 class="">Pawarit Phenkhoksung</h1>
+<h2 class="">Software Engineer</h2>
+<h3 class="">NVC</h3>
+<p class=""></p>
+</section>
+</main>
+
+<footer class=""></footer>
 
 
 </body>
