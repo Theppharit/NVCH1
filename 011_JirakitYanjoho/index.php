a0f6ccef69324ae5a1c635bea52bcde8c6d2a462
JirakitYanjoho <naokigmk23@gmail.com>
2025-10-29 11:17:45 +0700

    s

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -41,7 +41,7 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <section class="" id="section2">
 
-    <h1 class="">หัวข้อที่ 2</h1>
+    <h1 class="section-head">หัวข้อที่ 2</h1>
 <h2 class="">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
@@ -51,7 +51,7 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <section class="" id="section3">
 
-    <h1 class="">หัวข้อที่ 3</h1>
+    <h1 class="section-head">หัวข้อที่ 3</h1>
 <h2 class="">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">ห่วววววว</p>
