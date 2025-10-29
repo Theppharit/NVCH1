2faebb9c4f7df86ccde8393d928cd8f8287a1888
supansacake2006-code <supansacake2006@gmail.com>
2025-10-29 10:37:31 +0700

    c

--- 07_Supansa/index.php
+++ 07_Supansa/index.php
@@ -17,7 +17,7 @@
 
 
 <section class="" id=""></section>
-<h1 class="">supansa wisetsai</h1>
+<h1 class="">หัวข้อที่1</h1>
 <h2 class="">software Engineer</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis itaque rerum dignissimos laboriosam. Incidunt adipisci consectetur aut deserunt ab. Nesciunt pariatur maiores quidem ipsam at recusandae nisi iusto expedita nostrum.
@@ -27,7 +27,38 @@ Quibusdam commodi est maiores a similique. Doloremque veniam iste iusto sint nul
 Unde iste, impedit commodi iure laborum magnam consequatur placeat ipsum quae ab, aut nulla ea libero aliquid officiis et soluta cumque perspiciatis blanditiis consequuntur nemo necessitatibus facere harum aperiam? Blanditiis? ipsum dolor sit amet consectetur adipisicing elit. Corrupti minus doloribus dignissimos, inventore temporibus sit obcaecati porro, sint fugit repudiandae sed minima tenetur nulla, accusantium explicabo autem quam assumenda rerum?
 Nisi, consequuntur maiores fugit velit debitis inventore atque mollitia? Cumque, fuga. Temporibus nostrum molestiae eveniet mollitia provident laudantium, esse, tempora blanditiis ea nihil magni reprehenderit ipsa voluptates. Ullam, iure cum?</p>
 
-<section class="" id=""></section>
+</section>
+
+section class="" id=""></section>
+<h1 class="">หัวข้อที่2</h1>
+<h2 class="">software Engineer</h2>
+<h3 class="">NVC</h3>
+<p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis itaque rerum dignissimos laboriosam. Incidunt adipisci consectetur aut deserunt ab. Nesciunt pariatur maiores quidem ipsam at recusandae nisi iusto expedita nostrum.
+Nesciunt ea voluptates vel, laudantium expedita omnis nam suscipit molestiae, animi, itaque nulla praesentium culpa! Sit architecto vel id commodi, aut sint obcaecati iusto magni, accusamus nobis ullam iure? Quasi.
+Esse fugit unde, quibusdam odio impedit vel neque. Minus eligendi a omnis, est provident error rem sed fugiat necessitatibus autem, odit eaque repellat eveniet totam, commodi dignissimos cupiditate aspernatur? Accusantium?
+Quibusdam commodi est maiores a similique. Doloremque veniam iste iusto sint nulla laboriosam! Nostrum, amet magnam, placeat cumque nemo, at et nulla modi eveniet voluptatem ullam similique delectus eligendi magni!
+Unde iste, impedit commodi iure laborum magnam consequatur placeat ipsum quae ab, aut nulla ea libero aliquid officiis et soluta cumque perspiciatis blanditiis consequuntur nemo necessitatibus facere harum aperiam? Blanditiis? ipsum dolor sit amet consectetur adipisicing elit. Corrupti minus doloribus dignissimos, inventore temporibus sit obcaecati porro, sint fugit repudiandae sed minima tenetur nulla, accusantium explicabo autem quam assumenda rerum?
+Nisi, consequuntur maiores fugit velit debitis inventore atque mollitia? Cumque, fuga. Temporibus nostrum molestiae eveniet mollitia provident laudantium, esse, tempora blanditiis ea nihil magni reprehenderit ipsa voluptates. Ullam, iure cum?</p>
+
+</section>
+
+
+section class="" id=""></section>
+<h1 class="">หัวข้อที่3</h1>
+<h2 class="">software Engineer</h2>
+<h3 class="">NVC</h3>
+<p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis itaque rerum dignissimos laboriosam. Incidunt adipisci consectetur aut deserunt ab. Nesciunt pariatur maiores quidem ipsam at recusandae nisi iusto expedita nostrum.
+Nesciunt ea voluptates vel, laudantium expedita omnis nam suscipit molestiae, animi, itaque nulla praesentium culpa! Sit architecto vel id commodi, aut sint obcaecati iusto magni, accusamus nobis ullam iure? Quasi.
+Esse fugit unde, quibusdam odio impedit vel neque. Minus eligendi a omnis, est provident error rem sed fugiat necessitatibus autem, odit eaque repellat eveniet totam, commodi dignissimos cupiditate aspernatur? Accusantium?
+Quibusdam commodi est maiores a similique. Doloremque veniam iste iusto sint nulla laboriosam! Nostrum, amet magnam, placeat cumque nemo, at et nulla modi eveniet voluptatem ullam similique delectus eligendi magni!
+Unde iste, impedit commodi iure laborum magnam consequatur placeat ipsum quae ab, aut nulla ea libero aliquid officiis et soluta cumque perspiciatis blanditiis consequuntur nemo necessitatibus facere harum aperiam? Blanditiis? ipsum dolor sit amet consectetur adipisicing elit. Corrupti minus doloribus dignissimos, inventore temporibus sit obcaecati porro, sint fugit repudiandae sed minima tenetur nulla, accusantium explicabo autem quam assumenda rerum?
+Nisi, consequuntur maiores fugit velit debitis inventore atque mollitia? Cumque, fuga. Temporibus nostrum molestiae eveniet mollitia provident laudantium, esse, tempora blanditiis ea nihil magni reprehenderit ipsa voluptates. Ullam, iure cum?</p>
+
+</section>
+
+
+
+
 
 
 
