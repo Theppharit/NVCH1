82439f947f7c6b6608e50db02f4b70fa6f04d45c
JirakitYanjoho <naokigmk23@gmail.com>
2025-10-29 11:02:46 +0700

    a

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -5,14 +5,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./assels/style.css">
+
     <title>จอมมาร</title>
-<style>
-    .color {
-        color: .bleu;
-    }
-
-</style>
-
 </head>
 
 <body>
