fde9b8cc785ddd21fa04b7a0fdc5391abe1c66f3
mahathepsin <studyno1mahathep@gmail.com>
2025-10-29 11:07:15 +0700

    1

--- 05_Pawarit/page.php
+++ 05_Pawarit/page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pawarit</title>
-
+    <link rel="stylesheet" href="">
 
 
 
@@ -33,7 +33,7 @@
 <section class="" id="section1">
 
 
-<h1 class="">หัวข้อที่ 1</h1>
+<h1 class=".section-head">หัวข้อที่ 1</h1>
 <h2 class="">Software Engineer</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum beatae adipisci nisi! Fuga dignissimos commodi ipsam harum deserunt? Porro voluptatem explicabo molestiae atque eos suscipit eius aspernatur nostrum, officiis quasi.
