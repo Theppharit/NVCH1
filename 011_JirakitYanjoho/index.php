f13b2daf5ddbed51c64238dbf293f96fd3c69f02
JirakitYanjoho <naokigmk23@gmail.com>
2025-10-29 10:45:59 +0700

    5

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -10,27 +10,38 @@
 
 <body>
  
-<nav class=""></nav>
+
+
+<nav class="">
+    <a href="#section1" class="">หัวข้อที่ 2</a>
+    <a href="#section2" class="">หัวข้อที่ 1</a>
+    <a href="#section3" class="">หัวข้อที่ 3</a>
+
+</nav>
+
+
 
 <main class="">
 
-<section class="" id="">
+<section class="" id="section1">
     <h1 class="">หัวข้อที่ 1</h1>
 <h2 class="">ห่ะะ</h2>
 <h3 class="">NVC</h3>
-<p class="">ห่วววววว</p>
+<p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
+Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
 </section>
 
 
-<section class="" id="">
+<section class="" id="section2">
     <h1 class="">หัวข้อที่ 2</h1>
 <h2 class="">ห่ะะ</h2>
 <h3 class="">NVC</h3>
-<p class="">ห่วววววว</p>
+<p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
+Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
 </section>
 
 
-<section class="" id="">
+<section class="" id="section3">
     <h1 class="">หัวข้อที่ 3</h1>
 <h2 class="">ห่ะะ</h2>
 <h3 class="">NVC</h3>
