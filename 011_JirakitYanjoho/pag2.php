a75b44e1500df63e331a33288cb2345a7e96138f
JirakitYanjoho <naokigmk23@gmail.com>
2025-10-29 11:23:56 +0700

    Update pag2.php

--- 011_JirakitYanjoho/pag2.php
+++ 011_JirakitYanjoho/pag2.php
@@ -1,18 +1,70 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<link rel="stylesheet" href="./assets/style.css">
+
+    <title>จอมมาร</title>
 </head>
+
 <body>
-    
+ 
+
+
 <nav class="">
 
 <a href="./index.php" class="">หน้าแรก</a>
-<a href="" class=""></a>
+<a href="./pag2.php" class="">หน้าที่ 2</a>
+
+    <a href="#section1" class="">หัวข้อที่ 1</a>
+    <a href="#section2" class="">หัวข้อที่ 2</a>
+    <a href="#section3" class="">หัวข้อที่ 3</a>
 
 </nav>
 
+
+
+<main class="">
+
+<section class="" id="section1">
+
+    <h1 class="section-head">หัวข้อที่ 1</h1>
+<h2 class="">ห่ะะ</h2>
+<h3 class="">NVC</h3>
+<p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
+Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
+</section>
+
+
+<section class="" id="section2">
+
+    <h1 class="section-head">หัวข้อที่ 2</h1>
+<h2 class="">ห่ะะ</h2>
+<h3 class="">NVC</h3>
+<p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
+Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
+</section>
+
+
+<section class="" id="section3">
+
+    <h1 class="section-head">หัวข้อที่ 3</h1>
+<h2 class="">ห่ะะ</h2>
+<h3 class="">NVC</h3>
+<p class="">ห่วววววว</p>
+</section>
+
+
+
+
+</main>
+
+<footer class=""></footer>
+
 </body>
+
+
 </html>
