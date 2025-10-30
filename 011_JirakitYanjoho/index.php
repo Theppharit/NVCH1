8eb56bdea4f46eaaa4f572764234db4f8fa4ed0e
JirakitYanjoho <naokigmk23@gmail.com>
2025-10-30 14:15:37 +0700

    a

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -40,12 +40,12 @@
 
 <!-- section1 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 1</h1>
+    <h1 class="section-head2">หัวข้อที่ 1</h1>
 </div>
 
 <section class="section-contianer" id="section1">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
@@ -53,12 +53,12 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <!-- section2 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 2</h1>
+    <h1 class="section-head2">หัวข้อที่ 2</h1>
 </div>
 
 <section class="section-contianer" id="section2">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
@@ -66,12 +66,12 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <!-- section3 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 3</h1>
+    <h1 class="section-head2">หัวข้อที่ 3</h1>
 </div>
 
 <section class="section-contianer" id="section2">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
--- 011_JirakitYanjoho/pag2.php
+++ 011_JirakitYanjoho/pag2.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
 <!-- impor style.css -->
 <link rel="stylesheet" href="./assets/style.css">
 
@@ -17,14 +17,19 @@
  
 
 <!-- Navbar -->
-<nav class="">
+<nav class="navbar">
 
-<a href="./index.php" class="">หน้าแรก</a>
-<a href="./pag2.php" class="">หน้าที่ 2</a>
+<div class=" ">
+    <a href="./index.php" class="">หน้าแรก</a>
+    <a href="./pag2.php" class="">หน้าที่ 2</a>
+</div>
 
+<div class=" ">
     <a href="#section1" class="">หัวข้อที่ 1</a>
     <a href="#section2" class="">หัวข้อที่ 2</a>
     <a href="#section3" class="">หัวข้อที่ 3</a>
+</div>
+    
 
 </nav>
 
@@ -35,12 +40,12 @@
 
 <!-- section1 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 1</h1>
+    <h1 class="section-head2">หัวข้อที่ 1</h1>
 </div>
 
 <section class="section-contianer" id="section1">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
@@ -48,12 +53,12 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <!-- section2 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 2</h1>
+    <h1 class="section-head2">หัวข้อที่ 2</h1>
 </div>
 
 <section class="section-contianer" id="section2">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
@@ -61,12 +66,12 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <!-- section3 Title-->
 <div class="div-head">
-    <h1 class="section-head">หัวข้อที่ 3</h1>
+    <h1 class="section-head2">หัวข้อที่ 3</h1>
 </div>
 
 <section class="section-contianer" id="section2">
 
-<h2 class="section-head2">ห่ะะ</h2>
+<h2 class="section-head">ห่ะะ</h2>
 <h3 class="">NVC</h3>
 <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
 Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem id velit, dolor architecto. Quae, vero quas eligendi totam velit explicabo, harum ea magnam eos, vitae numquam doloribus neque nemo dolorem?</p>
