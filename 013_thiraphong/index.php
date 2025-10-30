7f109a619818b8e9ada0a65829914a6ca8c36896
kawzx123 <tonkawchu@gmail.com>
2025-10-30 15:06:49 +0700

    +

--- 013_thiraphong/index.php
+++ 013_thiraphong/index.php
@@ -2,21 +2,9 @@
 <html lang="en">
 
 
+<!-- Head -->
+<?php include('./include/head.php') ?>
 
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- import styles.css -->
-    <link rel="stylesheet" href="./assets/style.css">
-    
-   <!-- website name -->
-    <title>Thiraphong</title>
-
-
-
-</head>
 
 
 
@@ -24,23 +12,7 @@
 <body>
  
 <!-- Navbar -->
-<nav class="navbar">
-
-<div class="div div-navbar">
-<a href="./index.php" class="a-navbar">หน้าแรก</a>
-<a href="./page.php" class="a-navbar">หน้าที่ 2</a>
-</div>
-
-
-
-<div class="div div-navbar">
-<a href="#section1" class="a-navbar">หัวข้อที่ 1</a>
-<a href="#section2" class="a-navbar">หัวข้อที่ 2</a>
-<a href="#section3" class="a-navbar">หัวข้อที่ 3</a>
-</div>
-
-
-</nav>
+<?php include('./include/navbar.php') ?>
 
 
 <!-- Main -->
