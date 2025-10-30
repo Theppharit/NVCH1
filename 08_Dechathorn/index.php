3dd6e55ca9fd19e5f3af602f2f40170c6b896310
tonkr0611044154-wq <tonkr0611044154@gmail.com>
2025-10-30 15:41:18 +0700

    .

--- 08_Dechathorn/index.php
+++ 08_Dechathorn/index.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- inport style.css -->
-    <link rel="stylesheet" href="./assets/style.css">
-
-    <!-- Wedstis name -->
-    <title>Dechathorn Ronkrathok</title>
-</head>
-
+<?php include('./include/head.php')?>
 
 <body>
    
    <!-- nav bar -->
    <nav class="navbar">
 
-      <div class="div div-navbar">
-         <a href="./index.php" class="a-navbar">หน้าแรก</a>
-         <a href="./index page2.php" class="a-navbar">หน้าที่2</a>
-      </div>
-
-       <div class="div div-navbar">
-         <a href="#หน้าที่1" class="a-navbar">ข้อมูลที่1</a>
-         <a href="#หน้าที่2" class="a-navbar">ข้อมูลที่2</a>
-         <a href="#หน้าที่3" class="a-navbar">ข้อมูลที่3</a>
-
-        </div>
+      <?php include('./include/navbar.php')?>
    
 
       
@@ -160,7 +139,7 @@
 
 
 
-    <footer class=""></footer>
+    <?php include('./include/footer.php')?>
 
 
 </body>
