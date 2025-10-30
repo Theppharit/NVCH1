15aabb5e7c2e155688561d36e5d4a717546cb91e
supansacake2006-code <jakjakgapat@gmail.com>
2025-10-30 14:10:39 +0700

    huh

---
+++ 01_JAKGAPAT/page2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./accets/style.css">
+    <title>Pheeraphat </title>
+    <style>
+        .color{
+            color:white;
+        }
+    </style>
+</head>
+
+
+<body>  
+
+<nav class="">
+
+    <a href="./index.php" class="">หน้าหลัก</a>
+    <a href="./pase2.php" class="">หน้าที่สอง</a>
+    <a href="#section1" class="herf">หัวที่ 1</a>
+    <a href="#section2" class="herf">หัวที่ 2</a>
+    <a href="#section3" class="herf">หัวที่ 3</a>
+</nav>
+
+<main class="">
+
+<section class="" id="section1">
+    <h1 class="">Pheeraphat1</h1>
+    <h2 class="">bo</h1>
+    <h3 class="">age 19</h1>
+    <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident repudiandae asperiores quidem minus quia nesciunt in aliquam aperiam, maiores sunt! Sapiente quis necessitatibus nesciunt veritatis fugit ad distinctio facere harum!
+    Molestiae reiciendis impedit repudiandae dolores cupiditate sed, labore sunt. Adipisci quo nihil consequuntur, incidunt provident voluptatum. Unde magni voluptate, velit omnis nulla iste. Veritatis voluptate impedit id ut neque eius!</p>
+
+    </section>
+    <section class="" id="">
+    <h1 class="">Pheeraphat2</h1>
+    <h2 class="">bo</h1>
+    <h3 class="">age 19</h1>
+    <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident repudiandae asperiores quidem minus quia nesciunt in aliquam aperiam, maiores sunt! Sapiente quis necessitatibus nesciunt veritatis fugit ad distinctio facere harum!
+    Molestiae reiciendis impedit repudiandae dolores cupiditate sed, labore sunt. Adipisci quo nihil consequuntur, incidunt provident voluptatum. Unde magni voluptate, velit omnis nulla iste. Veritatis voluptate impedit id ut neque eius!</p>
+
+    </section>
+    <section class="" id="">
+    <h1 class="">Pheeraphat3</h1>
+    <h2 class="">bo</h1>
+    <h3 class="">age 19</h1>
+    <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident repudiandae asperiores quidem minus quia nesciunt in aliquam aperiam, maiores sunt! Sapiente quis necessitatibus nesciunt veritatis fugit ad distinctio facere harum!
+    Molestiae reiciendis impedit repudiandae dolores cupiditate sed, labore sunt. Adipisci quo nihil consequuntur, incidunt provident voluptatum. Unde magni voluptate, velit omnis nulla iste. Veritatis voluptate impedit id ut neque eius!</p>
+
+</section>
+
+
+</main>
+
+
+
+<footer class=""></footer>
+
+
+</body>
+
+</html>
