25dae4d208e3c50d7bbaf7371869f1f77b33ef73
Th.To. <theppharit7@gmail.com>
2025-10-30 14:38:21 +0700

    +

--- 00_Theppharit/index.php
+++ 00_Theppharit/index.php
@@ -23,12 +23,12 @@
     <!-- Navbar -->
     <nav class="navbar">
 
-    <div class="div div-navbar">
+    <div class="div-navbar">
         <a href="./index.php" class="a-navbar">หน้าแรก</a>
         <a href="./page2.php" class="a-navbar">หน้าที่ 2</a>
     </div>
 
-    <div class="div div-navbar">
+    <div class="div-navbar">
         <a href="#section1" class="a-navbar">หัวข้อที่ 1</a>
         <a href="#section2" class="a-navbar">หัวข้อที่ 2</a>
         <a href="#section3" class="a-navbar">หัวข้อที่ 3</a>
