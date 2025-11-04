1714b457b84abb731bb3a03883a618674fde71cc
JirakitYanjoho <naokigmk23@gmail.com>
2025-11-04 16:37:58 +0700

    a

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -22,16 +22,14 @@
 </div>
 
 <section class="section-contianer" id="section1">
-    <a href="https://youtu.be/qthxa7m_B4E?si=cIMg_VEeb8WBlrmf"><h1>อย่ากดเข้าไปนะ!</h1></a>
 
 <iframe width="560" height="315" 
     src="https://www.youtube.com/embed/qthxa7m_B4E" 
         title="YouTube video player" frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                    <h1>อย่ากดเข้าไปนะ!</h1>
 </iframe>
-
-
         <img src="./img/arknights-dance-kaltsit-dance.gif" alt=" " width=250 height=250>
 
              <h2 class="section-head">ห่ะะ</h2>
@@ -47,7 +45,9 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 </div>
 
 <section class="section-contianer" id="section2">
-    <a href="https://youtu.be/Rc2k_8skxtI?si=URcCllCbzm5G03ek"><h1>MeMe</h1></a>
+
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/kIte0CuJZ5E?si=__ZUSgbP3t_W3qpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <img src="./img/unnamed.webp" alt=" " width=250 height=250>
             <h2 class="section-head">ห่ะะ</h2>
                  <h3 class="">NVC</h3>
@@ -62,8 +62,10 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 </div>
 
 <section class="section-contianer" id="section2">
-    <a href="https://youtu.be/KpDIsxgdf-Y?si=fU-TZZ0vJ60egLzy"><h1>MeMe</h1></a>
-        <img src="./img/12.webp" alt=" " width=250 height=250>
+    
+    
+<iframe width="560" height="315" src="https://www.youtube.com/embed/UnktCDi-BVs?si=vGQBOG3LaFMNfUbK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <img src="./img/12.webp" alt=" " width=350 height=250>
             <h2 class="section-head">ห่ะะ</h2>
                 <h3 class="">NVC</h3>
                      <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
