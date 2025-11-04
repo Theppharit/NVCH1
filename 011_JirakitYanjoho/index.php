cee411d7d43420b5bbafa3affc0654ffca25c949
JirakitYanjoho <naokigmk23@gmail.com>
2025-11-04 16:05:43 +0700

    Update index.php

--- 011_JirakitYanjoho/index.php
+++ 011_JirakitYanjoho/index.php
@@ -23,7 +23,7 @@
 
 <section class="section-contianer" id="section1">
     <a href="https://youtu.be/qthxa7m_B4E?si=cIMg_VEeb8WBlrmf"><h1>อย่ากดเข้าไปนะ!</h1></a>
-        <img src="./img/arknights-dance-kaltsit-dance.gif" alt=" " width=300 height=150>
+        <img src="./img/arknights-dance-kaltsit-dance.gif" alt=" " width=250 height=250>
 
              <h2 class="section-head">ห่ะะ</h2>
                 <h3 class="">NVC</h3>
@@ -39,7 +39,7 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <section class="section-contianer" id="section2">
     <a href="https://youtu.be/Rc2k_8skxtI?si=URcCllCbzm5G03ek"><h1>MeMe</h1></a>
-        <img src="./img/unnamed.webp" alt=" " width=300 height=150>
+        <img src="./img/unnamed.webp" alt=" " width=250 height=250>
             <h2 class="section-head">ห่ะะ</h2>
                  <h3 class="">NVC</h3>
                         <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
@@ -54,7 +54,7 @@ Maiores iure, cumque veritatis, eos consequatur molestiae soluta exercitationem 
 
 <section class="section-contianer" id="section2">
     <a href="https://youtu.be/KpDIsxgdf-Y?si=fU-TZZ0vJ60egLzy"><h1>MeMe</h1></a>
-        <img src="./img/12.webp" alt=" " width=300 height=150>
+        <img src="./img/12.webp" alt=" " width=250 height=250>
             <h2 class="section-head">ห่ะะ</h2>
                 <h3 class="">NVC</h3>
                      <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, iure ducimus sapiente qui cum quibusdam tempora aperiam ipsum asperiores, quae fuga eligendi vero necessitatibus vel. Doloremque rem natus beatae ullam!
