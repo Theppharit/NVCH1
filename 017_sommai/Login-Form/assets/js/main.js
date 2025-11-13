4b4a674aae5c177a4d0b146dde1a837e439f690a
mai992 <sommai992za@gmail.com>
2025-11-13 13:35:54 +0700

    s

--- 017_sommai/Login-Form/assets/js/main.js
+++ 017_sommai/Login-Form/assets/js/main.js
@@ -1,4 +1,13 @@
 /*=============== HIDE & SHOW PASSWORD ===============*/
+const showHiddenPass = (password, eye ) =>{
+    const input = document.getElementById(password),
+          iconEye = document.getElementById(eye)
+    iconEye.addEventListener('click' , () => {
+        input.type ==='password' ? input.type = 'text'
+                                 : input.type = 'password'
+        iconEye.classlist.toggle('')
+    })      
+}
 
-
+showHiddenPass('loginPass','login Eye')
 /*=============== SWIPER IMAGES ===============*/
