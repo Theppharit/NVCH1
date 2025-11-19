edfc71c1ab8d9bba49493ed638aa952ce827aa7f
mai992 <sommai992za@gmail.com>
2025-11-19 11:43:08 +0700

    0

---
+++ 017_sommai/Delivery Website/assets/js/main.js
@@ -0,0 +1,31 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (navId, toggleId) => {
+    const nav = document.getElementById(navId),
+            toggle = document.getElementById(toggleId)
+
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu')
+    })
+}
+showMenu('nav-menu','nav-toggle')
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
+
+/*=============== SWIPER REVIEWS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
