1e0c3e60e048e8d98f0c55cb72f14f72a6746751
mahathepsin <studyno1mahathep@gmail.com>
2025-12-02 15:32:32 +0700

    2

--- 01_JAKGAPAT/Delivery Website/assets/js/main.js
+++ 01_JAKGAPAT/Delivery Website/assets/js/main.js
@@ -1,49 +1,15 @@
 /*=============== SHOW MENU ===============*/
-const showMenu = (navId, toggleId) =>{
-   const nav = document.getElementById(navId),
-         toggle = document.getElementById(toggleId)
 
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('show-menu')
-    })
-}
-showMenu('nav-menu','nav-toggle')
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
-const shadowHeader = () =>{
-    const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
 
-/*===============  SWIPER REVIEWS ===============*/ 
-const swiperReviews = new Swiper('.reviews__swiper',{ 
-  loop: true,
-  spaceBetween: 16,
-  grabCursor: true,
-  speed: 600,
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  }
-})
+/*=============== SWIPER REVIEWS ===============*/
+
+
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
