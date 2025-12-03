c04918571137b75ffd00d569bdefe109813a4bd5
mahathepsin <studyno1mahathep@gmail.com>
2025-12-03 11:18:26 +0700

    kl

--- 05_Pawarit/Delivery-Website-full/include/navbar.php
+++ 05_Pawarit/Delivery-Website-full/include/navbar.php
@@ -5,36 +5,35 @@
         </a>
 
         <div class="nav__menu" id="nav-menu">
-          <ul class="nav__list grid">
-            <li class="nav__item">
+          <ul class="nav__list">
+            <li>
               <a href="#home" class="nav__link active-link">Home</a>
             </li>
 
-            <li class="nav__item">
+            <li>
               <a href="#menu" class="nav__link">Menu</a>
             </li>
 
-            <li class="nav__item">
+            <li>
               <a href="#reviews" class="nav__link">Reviews</a>
             </li>
 
-            <li class="nav__item">
-              <a href="#app" class="nav__link">App</a>
+            <li>
+              <a href="#app" class="nav__link">Our App</a>
             </li>
 
-            <li class="nav__item">
-              <a href="#map" class="nav__link">Map</a>
+            <li>
+              <a href="#map" class="nav__link">Find Us</a>
             </li>
           </ul>
+
+          <!-- Theme button -->
+          <i class="ri-moon-fill nav__theme" id="theme-button"></i>
         </div>
 
-        <div class="nav__btns">
-          <!-- Theme change button -->
-          <i class="ri-moon-line change-theme" id="theme-button"></i>
-
-          <!-- Toggle menu button -->
-          <div class="nav__toggle" id="nav-toggle">
-            <i class="ri-menu-3-fill"></i>
-          </div>
+        <!-- Toggle button -->
+        <div class="nav__toggle" id="nav-toggle">
+          <i class="ri-menu-5-fill"></i>
         </div>
       </nav>
+    </header>
