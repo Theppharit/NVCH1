974e90706908315917996e73116e54ae872395e9
mahathepsin <studyno1mahathep@gmail.com>
2025-12-03 10:54:50 +0700

    kl;kl;kl;

--- 05_Pawarit/Delivery-Website-full/index.php
+++ 05_Pawarit/Delivery-Website-full/index.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <!--=============== FAVICON ===============-->
-    <link
-      rel="shortcut icon"
-      href="assets/img/favicon.png"
-      type="image/x-icon"
-    />
-
-    <!--=============== REMIXICONS ===============-->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.min.css"
-    />
-
-    <!--=============== SWIPER CSS ===============-->
-    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
-
-    <!--=============== CSS ===============-->
-    <link rel="stylesheet" href="assets/css/styles.css" />
-
-    <title>Responsive delivery website - Bedimcode</title>
-  </head>
+<?php include('include/head.php'); ?>
   <body>
     <!--==================== HEADER ====================-->
     
