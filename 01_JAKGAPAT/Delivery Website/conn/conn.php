4050666df7be823b233092e7332073f6b68a3c32
jakjakgapat-byte <jakjakgapat@gmail.com>
2025-12-03 16:13:58 +0700

    s

--- 01_JAKGAPAT/Delivery Website/conn/conn.php
+++ 01_JAKGAPAT/Delivery Website/conn/conn.php
@@ -3,5 +3,5 @@ $host ='localhost';
 $user ='root';
 $pass ='';
 $dbneme ='jakgapat';
-$conn = mysqli_connect($host,$user,$pas,$dbneme,);
+$conn = mysqli_connect($host,$user,$pass,$dbneme,);
 $conn -> set_charset('utf8');
