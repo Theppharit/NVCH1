243bcff28265b79efc01e6be39da276c651fb6fa
jakjakgapat-byte <jakjakgapat@gmail.com>
2025-12-04 14:50:46 +0700

    k

--- 01_JAKGAPAT/Delivery Website/index.php
+++ 01_JAKGAPAT/Delivery Website/index.php
@@ -1,134 +1,120 @@
-<?php include ('include/head.php')?>
-  <body>
-    <!--==================== HEADER ====================-->
-    <?php include ('include/navbar.php')?>
-  <body>
+<?php include('include/head.php') ?>
 
-    <!--==================== MAIN ====================-->
-    <main class="main">
-      <!--==================== HOME ====================-->
-      <section class="home section" id="home">
-        <div class="home__container container grid">
-          <div class="home__data">
-            <div>
-              <h1 class="home__title">
-                Enjoy Your Delicious <span>Food</span>
-              </h1>
+<body>
+  <!--==================== HEADER ====================-->
+  <?php include('include/navbar.php') ?>
 
-              <p class="home__description">
-                We'll delight your stomach with a variety of delicious
-                home-delivered food, delivered quickly and without delay.
-              </p>
+  <!--==================== MAIN ====================-->
+  <main class="main">
+    <!--==================== HOME ====================-->
+    <section class="home section" id="home">
+      <div class="home__container container grid">
+        <div class="home__data">
+          <div>
+            <h1 class="home__title">
+              Enjoy Your Delicious <span>Food</span>
+            </h1>
 
-              <a href="#menu" class="button">Order Now</a>
-            </div>
+            <p class="home__description">
+              We'll delight your stomach with a variety of delicious
+              home-delivered food, delivered quickly and without delay.
+            </p>
 
-            <div class="home__social">
-              <a
-                href="https://m.me/bedimcode"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-messenger-fill"></i>
-              </a>
-
-              <a
-                href="https://www.instagram.com/bedimcode/"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-instagram-fill"></i>
-              </a>
-
-              <a
-                href="https://api.whatsapp.com/send?phone=51123456789&text=Hello, more information!"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-whatsapp-fill"></i>
-              </a>
-
-              <a
-                href="https://web.telegram.org/"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-telegram-2-fill"></i>
-              </a>
-            </div>
+            <a href="#menu" class="button">Order Now</a>
           </div>
 
-          <div class="home__image">
-            <img src="assets/img/home-img.png" alt="image" class="home__img" />
+          <div class="home__social">
+            <a
+              href="https://m.me/bedimcode"
+              target="_blank"
+              class="home__link">
+              <i class="ri-messenger-fill"></i>
+            </a>
 
-            <div class="home__phone">
-              <div class="home__icon">
-                <i class="ri-phone-fill"></i>
-              </div>
+            <a
+              href="https://www.instagram.com/bedimcode/"
+              target="_blank"
+              class="home__link">
+              <i class="ri-instagram-fill"></i>
+            </a>
 
-              <address class="home__number">
-                Phone number:
-                <span>+321(00)123-45-67</span>
-              </address>
-            </div>
+            <a
+              href="https://api.whatsapp.com/send?phone=51123456789&text=Hello, more information!"
+              target="_blank"
+              class="home__link">
+              <i class="ri-whatsapp-fill"></i>
+            </a>
 
-            <p class="home__comment">
-              Fast delivery and friendly staff. The food was good and very tasty
-              10/10.
-            </p>
+            <a
+              href="https://web.telegram.org/"
+              target="_blank"
+              class="home__link">
+              <i class="ri-telegram-2-fill"></i>
+            </a>
           </div>
         </div>
-      </section>
 
-      <!--=======11============= SERVICE ====================-->
-      <section class="service section">
-        <h4 class="section__subtitle">OUR SERVICE</h4>
-        <h2 class="section__title">How Does It Work?</h2>
+        <div class="home__image">
+          <img src="assets/img/home-img.png" alt="image" class="home__img" />
 
-        <div class="service__container container grid">
-          <div class="service__card">
-            <img
-              src="assets/img/service-img-1.svg"
-              alt="image"
-              class="service__img"
-            />
+          <div class="home__phone">
+            <div class="home__icon">
+              <i class="ri-phone-fill"></i>
+            </div>
 
-            <h3 class="service__title">Easy To Order</h3>
-            <p class="service__description">
-              You only need a few steps in ordering food.
-            </p>
+            <address class="home__number">
+              Phone number:
+              <span>+321(00)123-45-67</span>
+            </address>
           </div>
 
-          <div class="service__card">
-            <img
-              src="assets/img/service-img-2.svg"
-              alt="image"
-              class="service__img"
-            />
-
-            <h3 class="service__title">Fastest Delivery</h3>
-            <p class="service__description">
-              Always delivered on time and even faster.
-            </p>
-          </div>
-
-          <div class="service__card">
-            <img
-              src="assets/img/service-img-3.svg"
-              alt="image"
-              class="service__img"
-            />
-
-            <h3 class="service__title">Best Quality</h3>
-            <p class="service__description">
-              Not only fast for us quality is also number one.
-            </p>
-          </div>
+          <p class="home__comment">
+            Fast delivery and friendly staff. The food was good and very tasty
+            10/10.
+          </p>
         </div>
-      </section>
+      </div>
+    </section>
 
-     <!--==================== FOOTER ====================-->
-     <?php include ('include/footer.php')?>
-    <?php include ('include/add-js.php')?>
-  </body>
+    <!--==================== SERVICE ====================-->
+    <?php
+
+    $sql_sv = "SELECT * FROM tbl_service";
+    $result_sv = mysqli_query($conn, $sql_sv);
+
+    ?>
+
+    <section class="service section">
+      <h4 class="section__subtitle">OUR SERVICE</h4>
+      <h2 class="section__title">How Does It Work?</h2>
+
+      <div class="service__container container grid">
+
+        <?php while ($row_sv = mysqli_fetch_assoc($result_sv)) { ?>
+
+          <div class="service__card">
+            <img
+              src="assets/img/<?= $row_sv['sv_img'] ?>"
+              alt="image"
+              class="service__img" />
+
+            <h3 class="service__title"><?= $row_sv['sv_title'] ?></h3>
+            <p class="service__description">
+              <?= $row_sv['sv_description'] ?>
+            </p>
+          </div>
+
+        <?php } ?>
+        
+      </div>
+    </section>
+  </main>
+
+  <!--==================== FOOTER ====================-->
+  <?php include('include/footer.php') ?>
+
+  <?php include('include/add-js.php') ?>
+
+</body>
+
 </html>
