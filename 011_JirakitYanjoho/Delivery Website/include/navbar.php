56dd5360aa8f14b4bf5cdacdc164e118636e9057
JirakitYanjoho <naokigmk23@gmail.com>
2025-12-11 14:54:43 +0700

    a

--- 011_JirakitYanjoho/Delivery Website/include/navbar.php
+++ 011_JirakitYanjoho/Delivery Website/include/navbar.php
@@ -38,6 +38,10 @@ $result_c = mysqli_query( $conn, $sql_c);
             <li>
               <a href="find-us.php#map" class="nav__link">Find Us</a>
             </li>
+            <li>
+              <a href="Log-from.php" class="nav__link">Log in</a>
+            </li>
+
           </ul>
 
           <!-- Theme button -->
