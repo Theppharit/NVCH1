427d7dd346f1ff3137909c108273f6c207c41f88
JirakitYanjoho <naokigmk23@gmail.com>
2025-12-18 14:22:24 +0700

    a

--- 011_JirakitYanjoho/Delivery Website/check/check-login.php
+++ 011_JirakitYanjoho/Delivery Website/check/check-login.php
@@ -6,7 +6,7 @@ require_once('../conn/conn.php');
 $me_mail = $_POST['me_mail'];
 $me_pass = $_POST['me_pass'];
 
-$sql = "SELECT * FROM tbl_member WHERE me_mail = '$me_mail' AND me_pass = '$me_pass'";
+$sql = "SELECT * FROM tbl_member WHERE me_mail = '$me_mail' AND me_pass = '$me_pass' ";
 $result = mysqli_query( $conn, $sql);
 
 if (mysqli_num_rows($result) == 1) {
