b2b1296449f1450b54576b761b657b110d8fe853
kawzx123 <tonkawchu@gmail.com>
2025-12-18 13:29:12 +0700

    +

--- 013_thiraphong/Delivery Website/check/checklogin.php
+++ 013_thiraphong/Delivery Website/check/checklogin.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once('../conn/conn.php')
+require_once('../conn/conn.php');
 
 $me_mail = $_POST['me_mail'];
 $me_pass = $_POST['me_pass'];
@@ -18,6 +18,8 @@ if (mysqli_num_rows($result) == 1) {
       $_SESSION["me_pass"] = $row["me_pass"];
       $_SESSION["me_name"] = $row["me_name"];
       $me_id = $_SESSION['me_id'];
+
+      header("Location: ../index.php");
 
 } else {
     header("Location: ../login-form.php");
