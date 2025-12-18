d2726844471b3647986b7829d01cd0df877202ee
punthiwa014 <ntkmice06@gmail.com>
2025-12-18 14:24:57 +0700

    +

--- 06_Panupong/Delivery Website/conn/conn.php
+++ 06_Panupong/Delivery Website/conn/conn.php
@@ -3,8 +3,9 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'Panupong';
+$dbname = 'panupong';
 
 $conn = mysqli_connect($host,$user,$pass,$dbname );
 
 $conn -> set_charset('utf8');
+
