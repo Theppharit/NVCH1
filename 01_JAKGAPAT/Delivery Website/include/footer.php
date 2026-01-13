6a265db437c157ef890841fadc7080f7886f566e
15thanakornboonsom-create <15thanakornboonsom@gmail.com>
2026-01-13 15:59:23 +0700

    F

--- 01_JAKGAPAT/Delivery Website/include/footer.php
+++ 01_JAKGAPAT/Delivery Website/include/footer.php
@@ -105,6 +105,6 @@
             </div>
          </div>
          <span class="footer__copy">
-             &#169; All Rights Reserved By SOMMAI HUMYAI
+             &#169; All Rights Reserved By JAKGAPAT
          </span>
       </footer>
