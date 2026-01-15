23b9fa27eca2b8033e6c5b3718d3f69308a2229d
tonkr0611044154-wq <tonkr0611044154@gmail.com>
2026-01-15 14:30:10 +0700

    s

--- 08_Dechathorn/Delivery Website/conn/conn.php
+++ 08_Dechathorn/Delivery Website/conn/conn.php
@@ -1,9 +1,12 @@
 <?php
 
+error_reporting(0);
+
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'dechathorn';
+$dbname = 'sommai';
 
-$conn = mysqli_connect($host, $user, $pass,$dbname);
+$conn = mysqli_connect($host,$user,$pass,$dbname );
+
 $conn -> set_charset('utf8');
