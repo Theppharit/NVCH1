a597dc4a48865b7c1f5a03c78c98874d64e11491
punthiwa014 <ntkmice06@gmail.com>
2026-01-15 13:26:37 +0700

    d

---
+++ 014_punthiwa/Delivery Website/include/add-menu.php
@@ -0,0 +1,22 @@
+<?php
+session_once
+include_once('../conn/conn.php');
+
+$m_name=$_POST['m_name'];
+$m_amount=$_POST['m_amount'];
+$m_price=$_POST['m_price'];
+
+$sql="INSERT INTO tbl_menu (m_name,m_amount,m_price,me_id)
+ VALUES ('$m_name','$m_amount','$m_price','me_id')";
+$result=mysqli_query($conn,$sql);
+
+if($result){
+   header("location:../menu.php");
+else{};
+
+}else{
+    header("Location: member/dashboard.php?me_id=$me_id");
+    exit();
+}
+
+ 
