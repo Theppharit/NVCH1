ec9a4e732c0dd1a282125b2d15abec646f72868a
xp <piyaphonn20549@gmail.com>
2026-01-15 15:07:45 +0700

    +

--- 09_Pheeraphat/Delivery Website/include/navbar.php
+++ 09_Pheeraphat/Delivery Website/include/navbar.php
@@ -40,10 +40,10 @@
                      <a href="Findus.php#map" class="nav__link">Find Us</a>
                   </li>
 
-                  <?php  if (isset($_SESSION['me_id'])) { ?>
+                  <?php  if (isset($me_id)) { ?>
                      
                      <li>
-                        <a href="#" class="nav__link"><?= $me_name ?> </a>
+                        <a href="member/dashboard.php?me_id=<?= $me_id ?>" class="nav__link"><?= $me_name ?></a>
                      </li>
                      
                      
