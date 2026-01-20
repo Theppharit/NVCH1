80aeb3f176ae476f0ebfbde00aa526c9478c3f38
8=== <studyno1mahathep@gmail.com>
2026-01-20 16:41:02 +0700

    ;

--- 06_Panupong/Delivery-Website-full/conn/conn.php
+++ 06_Panupong/Delivery-Website-full/conn/conn.php
@@ -5,7 +5,7 @@ error_reporting(0);
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'theppharit';
+$dbname = 'panupong';
 
 $conn = mysqli_connect($host, $user, $pass,$dbname);
 $conn -> set_charset('utf8');
