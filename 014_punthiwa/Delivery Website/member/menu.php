1702c701cb34b40bf89ab7915509cfd18a66e103
punthiwa014 <ntkmice06@gmail.com>
2026-01-21 16:50:33 +0700

    g

--- 014_punthiwa/Delivery Website/member/menu.php
+++ 014_punthiwa/Delivery Website/member/menu.php
@@ -1,65 +1,65 @@
-<?php include('include/head.php') ?>
+<?php include('head.php') ?>
 
-  <body>
+<body>
     <!--==================== HEADER ====================-->
-   <?php include('include/navbar.php') ?>
+    <?php include('navbar.php') ?>
 
-<main class="main">
+    <!--==================== MAIN ====================-->
+    <main class="main">
 
-
-
-     <section class="menu section" id="menu">
-     <?php
-
-    $sql_sv = "SELECT * FROM tbl_menu";
-    $result_sv = mysqli_query($conn, $sql_m);
-
-    ?>
-     
-     
-     <table class="table">
-           <thead>
-               <tr>
-                        <th class="th-menu">รหัสสินค้า</th>
-                        <th class="th-menu">ชื่อเมนู</th>
-                        <th class="th-menu">รูปภาพ</th>
-                        <th class="th-menu">ปริมาณ</th>
-                        <th class="th-menu">ราคา</th>
-                        <th class="th-menu">แก้ไข&ลบ</th>
-               </tr>
-           
-               <?php while ($row_sv = mysqli_fetch_assoc($result_sv)) { ?>
-
-           <tr>
-                        <td class="th-menu"><?  $row_m['m_id']; ?></td>
-                        <td class="th-menu"><?  $row_m['m_name']; ?></td>
-                        <td class="th-menu"><img src="../assets/img/<?  $row_m['m_imag']; ?>" ></td>
-                        <td class="th-menu"><?  $row_m['m_awount']; ?></td>
-                        <td class="th-menu"><?  $row_m['m_price']; ?></td>
-                        <td class="th-menu">แก้ไข&ลบ</td>
- </tr>
-      <?php } ?>
-           
-           
-           
-           
-           
-           
-            </thead>
-           <tbody>
-           
-           
-            
-              
-      </section>
+        <section class="menu section" id="menu">
 
 
+            <?php
+
+            $sql_m = "SELECT * FROM tbl_menu";
+            $result_m = mysqli_query($conn, $sql_m);
+
+            ?>
 
 
+            <table class="table">
 
-<!--==================== FOOTER ====================-->
-    <?php include('include/footer.php') ?>
+                <tr>
+                    <th class="th-menu">รหัสสินค้า</th>
+                    <th class="th-menu">ชื่อเมนู</th>
+                    <th class="th-menu">รูปภาพ</th>
+                    <th class="th-menu">ปริมาณ</th>
+                    <th class="th-menu">ราคา</th>
+                    <th class="th-menu">แก้ไข & ลบ</th>
+                </tr>
 
-   <?php include('include/add-js.php') ?> 
-  </body>
+                <?php while ($row_m = mysqli_fetch_assoc($result_m)) { ?>
+
+                    <tr>
+                        <td class="td-menu"><?= $row_m['m_id'] ?></td>
+                        <td class="td-menu"><?= $row_m['m_name'] ?></td>
+                        <td class="td-menu"><img src="../assets/img/<?= $row_m['m_img'] ?>" alt="" class="td-img"></td>
+                        <td class="td-menu"><?= $row_m['m_amount'] ?></td>
+                        <td class="td-menu"><?= $row_m['m_price'] ?></td>
+
+                        <td class="td-menu">
+
+                            <a href="#" class="a-td"><i class="ri-pencil-line"></i></a>
+                            <a href="#" class="a-td"><i class="ri-delete-bin-6-line"></i></a>
+
+                        </td>
+                    </tr>
+
+                <?php } ?>
+
+            </table>
+
+
+        </section>
+
+    </main>
+
+    <!--==================== FOOTER ====================-->
+    <?php include('../include/footer.php') ?>
+
+    <?php include('add-js.php') ?>
+
+</body>
+
 </html>
