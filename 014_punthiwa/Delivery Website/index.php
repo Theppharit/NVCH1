beda82b828b7c54b2a3c22b2bf1530bf4b27bb30
punthiwa014 <ntkmice06@gmail.com>
2026-01-21 17:48:10 +0700

    u

--- 014_punthiwa/Delivery Website/index.php
+++ 014_punthiwa/Delivery Website/index.php
@@ -1,273 +1,120 @@
 <?php include('include/head.php') ?>
 
-  <body>
-    <!--==================== HEADER ====================-->
-   <?php include('include/navbar.php') ?>
+<body>
+  <!--==================== HEADER ====================-->
+  <?php include('include/navbar.php') ?>
 
-    <!--==================== MAIN ====================-->
-    <main class="main">
-      <!--==================== HOME ====================-->
-      <section class="home section" id="home">
-        <div class="home__container container grid">
-          <div class="home__data">
-            <div>
-              <h1 class="home__title">
-                Enjoy Your Delicious <span>Food</span>
-              </h1>
+  <!--==================== MAIN ====================-->
+  <main class="main">
+    <!--==================== HOME ====================-->
+    <section class="home section" id="home">
+      <div class="home__container container grid">
+        <div class="home__data">
+          <div>
+            <h1 class="home__title">
+              Enjoy Your Delicious <span>Food</span>
+            </h1>
 
-              <p class="home__description">
-                We'll delight your stomach with a variety of delicious
-                home-delivered food, delivered quickly and without delay.
-              </p>
+            <p class="home__description">
+              We'll delight your stomach with a variety of delicious
+              home-delivered food, delivered quickly and without delay.
+            </p>
 
-              <a href="#menu" class="button">Order Now</a>
-            </div>
-
-            <div class="home__social">
-              <a
-                href="https://m.me/bedimcode"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-messenger-fill"></i>
-              </a>
-
-              <a
-                href="https://www.instagram.com/bedimcode/"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-instagram-fill"></i>
-              </a>
-
-              <a
-                href="https://api.whatsapp.com/send?phone=51123456789&text=Hello, more information!"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-whatsapp-fill"></i>
-              </a>
-
-              <a
-                href="https://web.telegram.org/"
-                target="_blank"
-                class="home__link"
-              >
-                <i class="ri-telegram-2-fill"></i>
-              </a>
-            </div>
+            <a href="#menu" class="button">Order Now</a>
           </div>
 
-          <div class="home__image">
-            <img src="assets/img/home-img.png" alt="image" class="home__img" />
+          <div class="home__social">
+            <a
+              href="https://m.me/bedimcode"
+              target="_blank"
+              class="home__link">
+              <i class="ri-messenger-fill"></i>
+            </a>
 
-            <div class="home__phone">
-              <div class="home__icon">
-                <i class="ri-phone-fill"></i>
-              </div>
+            <a
+              href="https://www.instagram.com/bedimcode/"
+              target="_blank"
+              class="home__link">
+              <i class="ri-instagram-fill"></i>
+            </a>
 
-              <address class="home__number">
-                Phone number:
-                <span>+321(00)123-45-67</span>
-              </address>
-            </div>
+            <a
+              href="https://api.whatsapp.com/send?phone=51123456789&text=Hello, more information!"
+              target="_blank"
+              class="home__link">
+              <i class="ri-whatsapp-fill"></i>
+            </a>
 
-            <p class="home__comment">
-              Fast delivery and friendly staff. The food was good and very tasty
-              10/10.
-            </p>
+            <a
+              href="https://web.telegram.org/"
+              target="_blank"
+              class="home__link">
+              <i class="ri-telegram-2-fill"></i>
+            </a>
           </div>
         </div>
-      </section>
 
-      <!--==================== SERVICE ====================-->
-      <section class="service section">
-        <h4 class="section__subtitle">OUR SERVICE</h4>
-        <h2 class="section__title">How Does It Work?</h2>
+        <div class="home__image">
+          <img src="assets/img/home-img.png" alt="image" class="home__img" />
 
-        <div class="service__container container grid">
-          <div class="service__card">
-            <img
-              src="assets/img/service-img-1.svg"
-              alt="image"
-              class="service__img"
-            />
+          <div class="home__phone">
+            <div class="home__icon">
+              <i class="ri-phone-fill"></i>
+            </div>
 
-            <h3 class="service__title">Easy To Order</h3>
-            <p class="service__description">
-              You only need a few steps in ordering food.
-            </p>
+            <address class="home__number">
+              Phone number:
+              <span>+321(00)123-45-67</span>
+            </address>
           </div>
+
+          <p class="home__comment">
+            Fast delivery and friendly staff. The food was good and very tasty
+            10/10.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <!--==================== SERVICE ====================-->
+    <?php
+
+    $sql_sv = "SELECT * FROM tbl_service";
+    $result_sv = mysqli_query($conn, $sql_sv);
+
+    ?>
+
+    <section class="service section">
+      <h4 class="section__subtitle">OUR SERVICE</h4>
+      <h2 class="section__title">How Does It Work?</h2>
+
+      <div class="service__container container grid">
+
+        <?php while ($row_sv = mysqli_fetch_assoc($result_sv)) { ?>
 
           <div class="service__card">
             <img
-              src="assets/img/service-img-2.svg"
+              src="assets/img/<?= $row_sv['sv_img'] ?>"
               alt="image"
-              class="service__img"
-            />
+              class="service__img" />
 
-            <h3 class="service__title">Fastest Delivery</h3>
+            <h3 class="service__title"><?= $row_sv['sv_title'] ?></h3>
             <p class="service__description">
-              Always delivered on time and even faster.
+              <?= $row_sv['sv_description'] ?>
             </p>
           </div>
 
-          <div class="service__card">
-            <img
-              src="assets/img/service-img-3.svg"
-              alt="image"
-              class="service__img"
-            />
+        <?php } ?>
+        
+      </div>
+    </section>
+  </main>
 
-            <h3 class="service__title">Best Quality</h3>
-            <p class="service__description">
-              Not only fast for us quality is also number one.
-            </p>
-          </div>
-        </div>
-      </section>
+  <!--==================== FOOTER ====================-->
+  <?php include('include/footer.php') ?>
 
-      <!--==================== MENU ====================-->
-      
+  <?php include('include/add-js.php') ?>
 
-      <!--==================== REVIEWS ====================-->
-      <section class="reviews section" id="reviews">
-        <div class="reviews__container container grid">
-          <div class="reviews__content">
-            <h4 class="section__subtitle">OUR REVIEWS</h4>
-            <h2 class="section__title">What They Say?</h2>
+</body>
 
-            <div class="reviews__swiper swiper">
-              <div class="swiper-wrapper">
-               
-              
-              <article class="reviews__card swiper-slide">
-                  <div class="reviews__profile">
-                    <img
-                      src="assets/img/reviews-img-1.png"
-                      alt="image"
-                      class="reviews__photo"
-                    />
-
-                    <div class="reviews__data">
-                      <h3 class="reviews__name">Emy Hawkins</h3>
-
-                      <div class="reviews__rating">
-                        <div class="reviews__stars">
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                        </div>
-
-                        <h3 class="reviews__number">5.0</h3>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p class="reviews__comment">
-                    Food is the best. Besides the many and delicious meals, the
-                    service is also very good, especially in the very fast
-                    delivery. I highly recommend food to you.
-                  </p>
-                </article>
-
-                <article class="reviews__card swiper-slide">
-                  <div class="reviews__profile">
-                    <img
-                      src="assets/img/reviews-img-2.png"
-                      alt="image"
-                      class="reviews__photo"
-                    />
-
-                    <div class="reviews__data">
-                      <h3 class="reviews__name">Aliz Doe</h3>
-
-                      <div class="reviews__rating">
-                        <div class="reviews__stars">
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                        </div>
-
-                        <h3 class="reviews__number">5.0</h3>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p class="reviews__comment">
-                    Food is the best. Besides the many and delicious meals, the
-                    service is also very good, especially in the very fast
-                    delivery. I highly recommend food to you.
-                  </p>
-                </article>
-
-                <article class="reviews__card swiper-slide">
-                  <div class="reviews__profile">
-                    <img
-                      src="assets/img/reviews-img-3.png"
-                      alt="image"
-                      class="reviews__photo"
-                    />
-
-                    <div class="reviews__data">
-                      <h3 class="reviews__name">Anna Wlhix</h3>
-
-                      <div class="reviews__rating">
-                        <div class="reviews__stars">
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                          <i class="ri-star-fill"></i>
-                        </div>
-
-                        <h3 class="reviews__number">5.0</h3>
-                      </div>
-                    </div>
-                  </div>
-
-                  <p class="reviews__comment">
-                    Food is the best. Besides the many and delicious meals, the
-                    service is also very good, especially in the very fast
-                    delivery. I highly recommend food to you.
-                  </p>
-                </article>
-              </div>
-
-              <!-- Navigation buttons -->
-              <div class="swiper-button-prev">
-                <i class="ri-arrow-left-s-line"></i>
-              </div>
-
-              <div class="swiper-button-next">
-                <i class="ri-arrow-right-s-line"></i>
-              </div>
-            </div>
-          </div>
-
-          <div class="reviews__image">
-            <img
-              src="assets/img/review-img.png"
-              alt="image"
-              class="reviews__img"
-            />
-          </div>
-        </div>
-      </section>
-
-      <!--==================== APP ====================-->
-      
-
-      <!--==================== MAP ====================-->
-      
-
-    <!--==================== FOOTER ====================-->
-    <?php include('include/footer.php') ?>
-
-   <?php include('include/add-js.php') ?> 
-  </body>
 </html>
--- 014_punthiwa/Delivery Website/member/add-menu.php
+++ 014_punthiwa/Delivery Website/member/add-menu.php
@@ -2,22 +2,25 @@
 session_start();
 include_once('../conn/conn.php');
 
-$m_name=$_POST['m_name'];
-$m_amount=$_POST['m_amount'];
-$m_price=$_POST['m_price'];
+$m_name = $_POST['m_name'];
+$m_amount = $_POST['m_amount'];
+$m_price = $_POST['m_price'];
 
-$sql="INSERT INTO tbl_menu (m_name,m_amount,m_price,me_id)
- VALUES 
- ('$m_name','$m_amount','$m_price','$me_id')";
-$result=mysqli_query($conn,$sql);
+$sql = "INSERT INTO tbl_menu (m_name, m_amount, m_price)
+VALUES
+('$m_name', '$m_amount', '$m_price')";
+$result = mysqli_query($conn, $sql);
 
-if($result){
-   header("location:../menu.php");
- exit();
+    if ($result) {
 
-}else{
-    header("Location: member/dashboard.php?me_id=$me_id");
-    exit();
-}
+        header("Location: ../menu.php");
+        exit();
+
+    } else {
+
+        header("Location: member/dashboard.php");
+        exit();
+
+    }
+
 ?>
- 
