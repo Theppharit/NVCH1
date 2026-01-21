5d2af22f97419aefdc963a0433cfce4654b7ede8
kawzx123 <tonkawchu@gmail.com>
2026-01-21 11:25:01 +0700

    +

--- 013_thiraphong/Delivery Website/member/menu.php
+++ 013_thiraphong/Delivery Website/member/menu.php
@@ -10,20 +10,53 @@
    <section class="menu section" id="menu">
 
 
+<?php  
 
+  $sql_m = "SELECT * FROM tbl_menu";
+  $result_m = mysqli_query($conn, $sql_m);
+  
+?> 
 
 <table class="table">
 
 
 
 <tr>
-    <th class="menu">รหัสสินค้า</th>
-    <th class="menu">ชื่อเมนู</th>
-    <th class="menu">รูปภาพ</th>
-    <th class="menu">ปริมาณ</th>
-    <th class="menu">ราคา</th>
-    <th class="menu">แก้ไข & ลบ</th>
+    <th class="th-menu">รหัสสินค้า</th>
+    <th class="th-menu">ชื่อเมนู</th>
+    <th class="th-menu">รูปภาพ</th>
+    <th class="th-menu">ปริมาณ</th>
+    <th class="th-menu">ราคา</th>
+    <th class="th-menu">แก้ไข & ลบ</th>
 </tr>
+
+  <?php while ($row_m = mysqli_fetch_assoc ($result_m)) { ?>
+
+
+<tr>
+    <td class="td-menu"><?= $row_m['m_id'] ?></td>
+    <td class="td-menu"><?= $row_m['m_name'] ?></td>
+    <td class="td-menu"><img src="../assets/img/<?= $row_m['m_img'] ?>" alt="" class="td-img"></td>
+    <td class="td-menu"><?= $row_m['m_amount'] ?></td>
+    <td class="td-menu"><?= $row_m['m_price'] ?></td>
+    <td class="td-menu"><?= $row_m['m_id'] ?></td>
+
+
+     <td class="td-menu">
+        <a href="#" class="td-menu"><i class="ri-pencil-line"></i>
+        <a href="#" class="td-menu"><i class="ri-delete-bin-6-line"></i>
+     </td>
+
+
+
+
+
+</tr>
+
+
+
+
+<?php } ?>  
 
 
 
