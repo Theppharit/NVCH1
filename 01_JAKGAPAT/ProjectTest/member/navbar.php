eede30dd4320c081d1f07e9b56cdfca0eb1bd91d
jakjakgapat-byte <jakjakgapat@gmail.com>
2026-01-28 21:10:10 +0700

    d

---
+++ 01_JAKGAPAT/ProjectTest/member/navbar.php
@@ -0,0 +1,68 @@
+  <?php  
+  
+  $sql_c = "SELECT * FROM tbl_company";
+  $result_c = mysqli_query($conn, $sql_c);
+  
+  ?>    
+      
+      
+      <header class="header" id="header">                                        
+         <nav class="nav container">
+            <a href="../index.php#home" class="nav__logo">
+               
+<?php while ($row_c = mysqli_fetch_assoc ($result_c)) { ?>  
+      <?= $row_c['c_logo'] ?> <span><?= $row_c['c_name']?></span>
+  
+<?php } ?>          
+               
+            </a>
+
+            <div class="nav__menu" id="nav-menu">
+                 <ul class="nav__list">
+                    
+                  <li>
+                     <a href="dashboard.php" class="nav__link">dashboard</a>
+                  </li>
+                    
+                  <li>
+                     <a href="menu.php" class="nav__link">Menu</a>
+                  </li> 
+                    
+         
+
+                  <?php  if (isset($me_id)) { ?>
+                     
+                     <li>
+                        <a href="member/dashboard.php?me_id=<?= $me_id ?>" class="nav__link"><?= $me_name ?></a>
+                     </li>
+                     
+                     
+                     <li>
+                        <a href="check/logout.php" class="nav__link">Logout</a>
+                     </li>
+                     
+
+
+                  <?php } else { ?>
+                     
+                     <li>
+                        <a href="login-form.php" class="nav__link">Login</a>
+                     </li>
+                     
+
+                  <?php } ?>
+                 
+            
+                  </li>
+                  </ul>
+
+                 <!-- Theme button -->
+                  <i class="ri-moon-fill nav__theme" id="theme-button"></i>
+            </div>
+                  
+            <!-- Toggle button -->
+             <div class="nav__toggle" id="nav-toggle">
+                  <i class="ri-menu-5-fill"></i>
+             </div>
+         </nav>
+      </header>
