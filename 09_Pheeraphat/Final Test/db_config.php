10291a1c5884db285a2dca4cd97dae0719f70620
Pheeraphat01 <kubibo007@gmail.com>
2026-01-29 20:23:43 +0700

    +

--- 09_Pheeraphat/Final Test/db_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "pheeraphat");
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-$conn->set_charset("utf8mb4");
